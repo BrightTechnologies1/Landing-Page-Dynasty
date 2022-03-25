e22f25bae2f0cbe12e6b97c4b1725eeb5da16cd5
BrightTechnologies1 <silascyrax@gmail.com>
2022-03-25 05:13:48 +0100

    secong commit

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -15,19 +15,17 @@ function Navbar() {
     <div className='navbar'>
       <button className='mobile-menu' onClick={() => setIsMobile(!isMobile)}>
         {isMobile ? (
-              <XLg style={{ color:"#000", width: "40px", height: "40px" }} />
+              <XLg style={{ color:"#000", width: "50px", height: "50px" }} />
           ) : (
-              <List style={{ color: "#000", width: "40px", height: "40px" }} />
+              <List style={{ color: "#000", width: "50px", height: "50px" }} />
           )}
       </button>
       <div className='nav-item'>
-          <ul>
-            <img src={dynastylogo} alt="" />
-            <nav className={isMobile ? "navMenu-mobile" : "navMenu" } onClick={() => setIsMobile(false)} >
-              <li id='register1'><a href="#">Get started</a></li>
-              <li id='register'><a href="#">Pitch Deck</a></li>
-            </nav>
-          </ul>
+          <img src={dynastylogo} alt="" />
+          <nav className={isMobile ? "navMenu-mobile" : "navMenu" } onClick={() => setIsMobile(false)} >
+            <li id='register1'><a href="#">Get started</a></li>
+            <li id='register'><a href="#">Pitch Deck</a></li>
+          </nav>
       </div>
     </div>
   );
